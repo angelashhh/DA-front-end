e339c3169c8901aca59832b27e57df784b64e17b
angelashhh <429502981@qq.com>
2021-04-27 00:00:46 +0100

    post intervals end point working fine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import fetchJson from './action.js'
+import * as ReactDOM from "react-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,24 +17,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
-const classes = useStyles();
-const [m, setM] = useState("");
-const [n, setN] = useState("");
-const intervals = {m: 2, n: 5}
-  return (
+    const classes = useStyles();
+    const [m, setM] = useState("");
+    const [n, setN] = useState("");
+    const intervals = {m: m, n: n};
+
+    const tradeAndReset = (intervals) => {
+        fetchJson('POST', 'http://localhost:8080/intervals', intervals)
+        setM("")
+        setN("")
+    }
+
+    const page = (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="digital-asset-logo" />
-      </header>
-      <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="m-input" label="Interval m (min)" onChange={(event) => setM(event.target.value)}/>
-        <TextField id="n-input" label="Interval n (min)" onChange={(event) => setN(event.target.value)}/>
-        <Button variant="contained" color="primary" onClick={() => {fetchJson('POST', '/intervals', intervals)}}>
-            Happy Trading!
-        </Button>
-      </form>
+        <header className="App-header">
+            <img src={logo} alt="digital-asset-logo"/>
+        </header>
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="m-input" label="Interval m (min)" onChange={(event) => setM(event.target.value)}/>
+            <TextField id="n-input" label="Interval n (min)" onChange={(event) => setN(event.target.value)}/>
+            <Button variant="contained" color="primary" onClick={() => tradeAndReset(intervals)}>
+                Happy Trading!
+            </Button>
+        </form>
     </div>
-  );
+    )
+
+    return page
 }
 
 export default App;
--- src/action.js
+++ src/action.js
@@ -1,21 +1,29 @@
-const fetchJson = (method, url, body) => fetch(url,
-    {
-     method,
-     body: JSON.stringify(body),
-     headers: null
-    })
-    .then((response) => {
-    const {ok, status, statusText} = response
-//    return isJson(response)
-//    ? response.json().then(json => [ok, json, status, statusText])
-//    : Promise.resolve([ok, {}, status, statusText])
-        return Promise.resolve([ok, {}, status, statusText])
-    })
-    .then(([ok, responseBody, status, statusText]) => {
-    if (ok) {return responseBody}
-    const error = {status, statusText, responseBody}
-    console.log('ERROR, response: ', error)
-    throw error
-    })
+const isJson = (response) => response.headers.get('content-type')?.includes('application/json');
+const buildHeaders = () => {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    return headers;
+}
+const fetchJson = (method, url, body) => {
+    console.log("haha", JSON.stringify(body))
+    const requestOptions = {
+        // mode: 'cors',
+        method: method,
+        headers: buildHeaders(),
+        body: JSON.stringify(body),
+    };
+    fetch(url, requestOptions)
+        .then((response) => {
+            console.log("response", response)
+            const {ok, status, statusText} = response
+            return isJson(response)
+                ? response.json().then(json => [ok, json, status, statusText])
+                : Promise.resolve([ok, {}, status, statusText])
+        })
+        .then(data => console.log("data:", data));
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+};
 
 export default fetchJson;
